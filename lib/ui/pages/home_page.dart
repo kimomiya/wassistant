3b73455f3d32e7067a14708901c61af3854286d6
MasterHiei <ruhrg.lcx@gmail.com>
2020-01-15 23:34:56 +0900

    [WIP]implement search delegate

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wassistant/ui/widgets/left_drawer.dart';
+import 'package:wassistant/ui/widgets/navigation_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -7,9 +8,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-      ),
+      appBar: const NavigationBar('Home'),
       body: Container(
         child: const Center(
           child: Text('Hello world!'),
---
+++ lib/ui/widgets/navigation_bar.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class NavigationBar extends StatelessWidget implements PreferredSizeWidget {
+  const NavigationBar(this._title);
+
+  final String _title;
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Container(
+      child: AppBar(
+        title: Text(_title),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: _PlayerSearchDelegate(),
+              );
+            },
+          ),
+        ],
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: theme.accentColor,
+        ),
+        textTheme: theme.textTheme,
+      ),
+    );
+  }
+}
+
+class _PlayerSearchDelegate extends SearchDelegate<String> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return <Widget>[
+      IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_close,
+          progress: transitionAnimation,
+        ),
+        onPressed: () {
+          query = '';
+          showSuggestions(context);
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () => close(context, null),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Container();
+  }
+}
