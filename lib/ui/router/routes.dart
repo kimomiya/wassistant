d4da0152c9c385e8dab681c45c5302cf496204e8
MasterHiei <ruhrg.lcx@gmail.com>
2020-01-19 18:04:32 +0900

    add logger

--- lib/ui/router/routes.dart
+++ lib/ui/router/routes.dart
@@ -1,6 +1,7 @@
 import 'package:fluro/fluro.dart';
 import 'package:wassistant/ui/router/router.dart';
 import 'package:wassistant/ui/router/route_handlers.dart';
+import 'package:wassistant/utils/logger.dart';
 
 class Routes {
   static const home = '/';
@@ -9,8 +10,7 @@ class Routes {
   static void configure() {
     router.notFoundHandler = Handler(
       handlerFunc: (context, params) {
-        // TODO(hiei): waiting for Firebase Crashlytics
-        print('No route for $context, with $params.');
+        logger.e('No route for $context, with $params.');
         return null;
       },
     );
