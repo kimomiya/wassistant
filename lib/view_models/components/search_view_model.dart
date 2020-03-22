5a27194d2d376e5e24cae4ef280b96620547a963
MasterHiei <ruhrg.lcx@gmail.com>
2020-03-22 17:28:10 +0900

    remove unnecessary files

--- lib/view_models/components/search_view_model.dart
+++
@@ -1,7 +0,0 @@
-// import 'package:wassistant/repository/palyer_repository.dart';
-
-// class SearchViewModel {
-//   const SearchViewModel(this.repository);
-
-//   final PlayerRepository repository;
-// }
